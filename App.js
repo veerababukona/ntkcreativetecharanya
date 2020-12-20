5d4814acb712a3aa96d6cd91c4df70a1bdb59e19
Veerababu <veerababu.kona@gmail.com>
2020-12-20 12:36:58 +0530

    Implemented react native vector icons and react naviagation and drawer navigator

--- App.js
+++ App.js
@@ -1,114 +1,123 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import Icon from 'react-native-vector-icons/Ionicons';
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App: () => React$Node = () => {
+const HomeScreen = ({ navigation }) => {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+       <Button
+          title="Go to Login"
+          onPress={() => navigation.navigate('Login')}
+        />
+    </View>
   );
-};
+}
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+const LoginScreen = ({ navigation }) => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Login Screen</Text>
+      <Button
+        title="Go to Home Screen"
+        onPress={() => navigation.navigate('Home')}
+      />
+       <Button
+              title="Go to Register Screen"
+              onPress={() => navigation.navigate('Register')}
+            />
+    </View>
+  );
+}
+
+const RegisterScreen = ({navigation}) => {
+    return (
+        <View>
+            <Text>Register Screen</Text>
+            <Button
+                title="Go to Home Sceen"
+                onPress={() => navigation.navigate('Home')}
+            />
+
+        </View>
+    );
+}
+
+
+const DetailsScreen = () => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+
+const HomeStack = createStackNavigator();
+
+const LoginStack = createStackNavigator();
+
+const Drawer = createDrawerNavigator();
+
+
+const HomeStackScreen = ({navigation }) => (
+    <HomeStack.Navigator screenOptions={{
+        headerStyle: {
+            backgroundColor: '#009387'
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+            fontWeight: 'bold'
+        }
+      }}>
+        <HomeStack.Screen name="Home" component={HomeScreen}
+        options = {{
+        headerLeft: () => (
+            <Icon.Button name="ios-menu" size={25} backgroundColor='#009387'
+            onPress={ () => navigation.openDrawer()}>
+            </Icon.Button>
+        )
+        }}/>
+      </HomeStack.Navigator>
+)
+
+const LoginStackScreen = ({navigation }) => (
+    <LoginStack.Navigator screenOptions={{
+        headerStyle: {
+            backgroundColor: '#009387'
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+            fontWeight: 'bold'
+        }
+      }}>
+        <LoginStack.Screen  name="Login" component={LoginScreen} options={{
+         title:'Login',
+         headerLeft: () => (
+            <Icon.Button name="ios-menu" size={25} backgroundColor="#009387"
+            onPress= {() => navigation.openDrawer()}>
+            </Icon.Button>
+         )
+        }} />
+      </LoginStack.Navigator>
+)
+
+
+const App = () => {
+  return (
+    <NavigationContainer>
+        <Drawer.Navigator initialRouteName="Home">
+            <Drawer.Screen name="Home" component={HomeStackScreen} />
+            <Drawer.Screen name="Login" component={LoginStackScreen} />
+        </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
 
 export default App;
